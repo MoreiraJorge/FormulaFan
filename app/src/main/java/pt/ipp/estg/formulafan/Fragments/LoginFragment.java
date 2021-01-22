e51719dbfedb3aa77131a6e26ae41daeba6b3d3e
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-01-22 00:43:09 +0000

    Adicionadas cores e icon

---
+++ app/src/main/java/pt/ipp/estg/formulafan/Fragments/LoginFragment.java
@@ -0,0 +1,39 @@
+package pt.ipp.estg.formulafan.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pt.ipp.estg.formulafan.R;
+
+
+public class LoginFragment extends Fragment {
+
+    private Context context;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return v;
+    }
+}
