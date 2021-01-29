e08bbde18f7a2bbbf87bd3593658523328c7358c
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-01-29 19:19:33 +0000

    Adicionada primeira versão de login

--- app/src/main/java/pt/ipp/estg/formulafan/Fragments/LoginFragment.java
+++ app/src/main/java/pt/ipp/estg/formulafan/Fragments/LoginFragment.java
@@ -6,16 +6,30 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import pt.ipp.estg.formulafan.MainActivity;
 import pt.ipp.estg.formulafan.R;
 
 
 public class LoginFragment extends Fragment {
 
     private Context context;
+
+    private EditText insertEmailField;
+    private EditText insertPassField;
+    private Button signInButton;
+
+    public LoginFragment() {
+    }
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -31,9 +45,55 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_login, container, false);
 
-        return v;
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        insertEmailField = view.findViewById(R.id.editTextTextEmailAddress);
+        insertPassField = view.findViewById(R.id.editTextTextPassword);
+        signInButton = view.findViewById(R.id.logInButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validateForm() == true){
+                    String email = insertEmailField.getText().toString();
+                    String password = insertPassField.getText().toString();
+                    ((MainActivity) context).signIn(email,password);
+                } else {
+                    Toast.makeText(context, "Input error!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    /**
+     * Method to validate Email
+     * and Password Input
+     * @return true if forms are valid, otherwise
+     * returns false
+     */
+    private boolean validateForm(){
+        boolean valid = true;
+
+        String email = insertEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            insertEmailField.setError("Required!");
+            valid = false;
+        } else {
+            insertEmailField.setError(null);
+        }
+
+        String password = insertPassField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            insertPassField.setError("Required!");
+            valid = false;
+        } else {
+            insertPassField.setError(null);
+        }
+
+        return valid;
     }
 }
--- app/src/main/java/pt/ipp/estg/formulafan/MainActivity.java
+++ app/src/main/java/pt/ipp/estg/formulafan/MainActivity.java
@@ -2,21 +2,36 @@ package pt.ipp.estg.formulafan;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import pt.ipp.estg.formulafan.Fragments.LoginFragment;
 import pt.ipp.estg.formulafan.Fragments.RaceFragment;
+import pt.ipp.estg.formulafan.Interfaces.ISessionListener;
 import pt.ipp.estg.formulafan.Utils.InternetUtil;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ISessionListener {
+
+    private static final String TAG = "SessionEmailPass";
 
     private Context context;
     private FragmentManager fragmentManager;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +54,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        RaceFragment raceFragment = new RaceFragment();
-        raceFragment.setArguments(getIntent().getExtras());
+        mAuth = FirebaseAuth.getInstance();
 
+        LoginFragment loginFragment = new LoginFragment();
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragmentContainer, raceFragment);
+        fragmentTransaction.add(R.id.fragmentContainer, loginFragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+
+                            RaceFragment raceFragment = new RaceFragment();
+                            raceFragment.setArguments(getIntent().getExtras());
+                            FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                                    .beginTransaction();
+                            fragmentTransaction.replace(R.id.fragmentContainer, raceFragment);
+                            fragmentTransaction.addToBackStack(null);
+                            fragmentTransaction.commit();
+
+                            Toast.makeText(context, "Login Success!",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(context, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void register(String email, String password) {
+
+    }
+
+    @Override
+    public void logout() {
+
     }
 }
