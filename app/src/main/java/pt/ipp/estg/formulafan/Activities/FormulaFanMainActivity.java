bb1396cff105b000ba07fcbb6025f1a245d4603d
DevMig <nerdxontheline@outlook.com>
2021-01-30 19:09:41 +0000

    Implementing Bottom Bar!

--- app/src/main/java/pt/ipp/estg/formulafan/Activities/FormulaFanMainActivity.java
+++ app/src/main/java/pt/ipp/estg/formulafan/Activities/FormulaFanMainActivity.java
@@ -1,42 +1,96 @@
 package pt.ipp.estg.formulafan.Activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import pt.ipp.estg.formulafan.Fragments.ProfileFragment;
+import pt.ipp.estg.formulafan.Fragments.RaceFragment;
+import pt.ipp.estg.formulafan.Fragments.ResultFragment;
+import pt.ipp.estg.formulafan.Interfaces.IProfileListener;
 import pt.ipp.estg.formulafan.R;
 
-public class FormulaFanMainActivity extends AppCompatActivity {
+public class FormulaFanMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, IProfileListener {
 
-    private Context context;
-    private Button logoutButton;
+    private FragmentManager fragmentManager;
     private FirebaseAuth firebaseAuth;
+    private RaceFragment raceFragment;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formula_fan_main);
-
-        context = this.getApplicationContext();
-
         firebaseAuth = FirebaseAuth.getInstance();
 
-        logoutButton = findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebaseAuth.signOut();
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.races);
 
+        raceFragment = new RaceFragment();
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentContainerMainUI, raceFragment);
+        fragmentTransaction.commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.profile) {
+            changeToProfileFragment();
+            return true;
+        }
+        else if(item.getItemId() == R.id.races) {
+            changeToRaceFragment();
+            return true;
+        } else {
+            changeToResultFragment();
+            return true;
+        }
+    }
+
+    private void changeToProfileFragment() {
+        ProfileFragment profileFragment = new ProfileFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerMainUI, profileFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void changeToRaceFragment() {
+        raceFragment = new RaceFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerMainUI, raceFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void changeToResultFragment() {
+        ResultFragment resultFragment = new ResultFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerMainUI, resultFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public void logOut() {
+        firebaseAuth.signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
