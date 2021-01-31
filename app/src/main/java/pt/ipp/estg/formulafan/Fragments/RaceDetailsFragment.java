6a3272a852b674f4f29f8dbd28c0539b6444a7cd
DevMig <nerdxontheline@outlook.com>
2021-01-31 22:30:51 +0000

    Implementing Fragment Logic!

--- app/src/main/java/pt/ipp/estg/formulafan/Fragments/RaceDetailsFragment.java
+++ app/src/main/java/pt/ipp/estg/formulafan/Fragments/RaceDetailsFragment.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import pt.ipp.estg.formulafan.Activities.FormulaFanMainActivity;
@@ -16,7 +19,10 @@ import pt.ipp.estg.formulafan.R;
 
 public class RaceDetailsFragment extends Fragment {
 
-    private TextView message;
+    private ConstraintLayout messageView;
+    private LinearLayout detailsView;
+    private TextView raceName;
+    private Toolbar toolbar;
     private Race currentRace;
 
     public RaceDetailsFragment() {
@@ -40,14 +46,24 @@ public class RaceDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_race_details, container, false);
-        message = view.findViewById(R.id.messageView);
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.detalhes_corrida);
+        messageView = view.findViewById(R.id.messageView);
+        raceName = view.findViewById(R.id.raceNameView);
+        detailsView = view.findViewById(R.id.detailsView);
         if (currentRace != null) {
-            message.setText(currentRace.raceName);
+            messageView.setVisibility(View.GONE);
+            detailsView.setVisibility(View.VISIBLE);
+            toolbar.setTitle(currentRace.raceName + " - Detalhes");
+            raceName.setText(currentRace.raceName);
         }
         return view;
     }
 
     public void updateRace(Race race) {
-        message.setText(race.raceName);
+        messageView.setVisibility(View.GONE);
+        detailsView.setVisibility(View.VISIBLE);
+        toolbar.setTitle(race.raceName + " - Detalhes");
+        raceName.setText(race.raceName);
     }
 }
