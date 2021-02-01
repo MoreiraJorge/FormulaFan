e851eac93d04dbe71642564eb7f24f9cc058ebdb
DevMig <nerdxontheline@outlook.com>
2021-02-01 01:41:43 +0000

    Implementing MapView!

--- app/src/main/java/pt/ipp/estg/formulafan/Fragments/RaceDetailsFragment.java
+++ app/src/main/java/pt/ipp/estg/formulafan/Fragments/RaceDetailsFragment.java
@@ -12,16 +12,32 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import pt.ipp.estg.formulafan.Activities.FormulaFanMainActivity;
+import pt.ipp.estg.formulafan.Models.Circuit;
 import pt.ipp.estg.formulafan.Models.Race;
 import pt.ipp.estg.formulafan.R;
 
-public class RaceDetailsFragment extends Fragment {
+public class RaceDetailsFragment extends Fragment implements OnMapReadyCallback {
 
+    private SupportMapFragment mapFragment;
+    private GoogleMap googleMap;
+    private FragmentActivity context;
     private ConstraintLayout messageView;
     private LinearLayout detailsView;
-    private TextView raceName;
+    private TextView seasonView;
+    private TextView roundView;
+    private TextView dateView;
+    private TextView circuitView;
     private Toolbar toolbar;
     private Race currentRace;
 
@@ -31,6 +47,7 @@ public class RaceDetailsFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        this.context = (FragmentActivity) context;
     }
 
     @Override
@@ -46,17 +63,30 @@ public class RaceDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_race_details, container, false);
+
         toolbar = (Toolbar) view.findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.detalhes_corrida);
+
         messageView = view.findViewById(R.id.messageView);
-        raceName = view.findViewById(R.id.raceNameView);
+        seasonView = view.findViewById(R.id.seasonView);
         detailsView = view.findViewById(R.id.detailsView);
+        roundView = view.findViewById(R.id.roundView);
+        dateView = view.findViewById(R.id.dateView);
+        circuitView = view.findViewById(R.id.circuitView);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapLocation);
+        mapFragment.getMapAsync(this);
+
         if (currentRace != null) {
             messageView.setVisibility(View.GONE);
             detailsView.setVisibility(View.VISIBLE);
             toolbar.setTitle(currentRace.raceName + " - Detalhes");
-            raceName.setText(currentRace.raceName);
+            seasonView.setText(" " + currentRace.season);
+            roundView.setText(" " + currentRace.round);
+            dateView.setText(" " + currentRace.date.toLocaleString());
+            circuitView.setText(" " + currentRace.circuit.circuitName);
         }
+
         return view;
     }
 
@@ -64,6 +94,34 @@ public class RaceDetailsFragment extends Fragment {
         messageView.setVisibility(View.GONE);
         detailsView.setVisibility(View.VISIBLE);
         toolbar.setTitle(race.raceName + " - Detalhes");
-        raceName.setText(race.raceName);
+        seasonView.setText(" " + race.season);
+        roundView.setText(" " + race.round);
+        dateView.setText(" " + race.date.toLocaleString());
+        circuitView.setText(" " + race.circuit.circuitName);
+        addMarker(race.circuit);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        if (currentRace != null) {
+            addMarker(currentRace.circuit);
+        }
+    }
+
+    private void addMarker(Circuit circuit) {
+        LatLng latLng = new LatLng(circuit.location.lat, circuit.location.lng);
+        googleMap.clear();
+        googleMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(circuit.circuitName)
+                .snippet(circuit.location.locality)
+        );
+        zoomToLocation(latLng);
+    }
+
+    private void zoomToLocation(LatLng latLng) {
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        googleMap.animateCamera(cameraUpdate);
     }
 }
