8adf7f6ab3ac03e17b1d94ba5d73ce42fc0b6bf2
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-02-05 23:23:00 +0000

    Persistent login

---
+++ app/src/main/java/pt/ipp/estg/formulafan/Activities/QuizActivity.java
@@ -0,0 +1,16 @@
+package pt.ipp.estg.formulafan.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipp.estg.formulafan.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
