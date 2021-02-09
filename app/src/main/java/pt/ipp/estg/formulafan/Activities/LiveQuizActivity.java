e32ce6ed6bb989312838d2558b75708945dd4776
DevMig <nerdxontheline@outlook.com>
2021-02-09 18:56:17 +0000

    Adding new fragment!

--- app/src/main/java/pt/ipp/estg/formulafan/Activities/LiveQuizActivity.java
+++ app/src/main/java/pt/ipp/estg/formulafan/Activities/LiveQuizActivity.java
@@ -1,29 +1,27 @@
 package pt.ipp.estg.formulafan.Activities;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import pt.ipp.estg.formulafan.Fragments.QuizFragment;
+import pt.ipp.estg.formulafan.Fragments.LiveQuizFragment;
 import pt.ipp.estg.formulafan.R;
 
 public class LiveQuizActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager;
-    private QuizFragment quizFragment;
+    private LiveQuizFragment liveQuizFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Test", "Here");
         setContentView(R.layout.activity_quiz);
-        quizFragment = new QuizFragment();
+        liveQuizFragment = new LiveQuizFragment();
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentQuizContainer, quizFragment);
+        fragmentTransaction.replace(R.id.fragmentQuizContainer, liveQuizFragment);
         fragmentTransaction.commit();
     }
 
---
+++ app/src/main/java/pt/ipp/estg/formulafan/Fragments/LiveQuizFragment.java
@@ -0,0 +1,24 @@
+package pt.ipp.estg.formulafan.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import pt.ipp.estg.formulafan.R;
+
+public class LiveQuizFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_live_quiz, container, false);
+    }
+}
